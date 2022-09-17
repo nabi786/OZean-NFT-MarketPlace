46ca3558a6c52ebaef4a8093f89c8f1f590d9ad1
nabi786 <rehanpardesi2018@gmail.com>
2022-09-17 11:02:13 +0500

    updated on 11:011 AM 17th September

--- routers/adminRegistration.js
+++ routers/adminRegistration.js
@@ -7,27 +7,20 @@ const auth = require('../middleware/auth')
 // create Admin
 router.post('/admin', admin.adminRegister)
 
-
 // update Admin
 router.patch('/admin', admin.updateAdmin)
-
-
 
 // delete Admin here
 router.delete('/admin', admin.deleteAdmin)
 
-
-
 // get all Admin
 // router.get('/admin', admin.getAllAdmins)
-
 
 // login admin
 router.post('/admin-login', admin.adminLogin)
 
-
 // getSingle Admin by walletAddress
-router.get('/getSingleAdmin', auth, admin.getSingleAdmin)
+router.get('/getSingleAdmin', admin.getSingleAdmin)
 
 
 // exporting module
