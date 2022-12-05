549b6671a8f055603dd4b90760db6a9cbd89bf89
nabi786 <rehanpardesi2018@gmail.com>
2022-12-05 13:13:08 +0500

    updated on 1:13 PM

--- routers/indexRoute.js
+++ routers/indexRoute.js
@@ -4,8 +4,8 @@ router = require('express').Router()
 // index Route 
 router.get('/', async (req,res)=>{
     try{
+        
         res.status(200).json({msg : "Eden Fort Backend working successfully" })
-
     }catch(err){
         res.status(500).json({msg : "server error"})
     }
